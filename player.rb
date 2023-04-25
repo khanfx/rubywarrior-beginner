a9c0ab39613654b9050687e9b8ed2b0ef7d99160
Fahd Khan <fahd@khanfx.com>
2023-04-24 23:21:15 -0500

    better points

--- player.rb
+++ player.rb
@@ -1,17 +1,24 @@
 MAX_HP = 20
 
 class Player
+
   def initialize
-    puts 'Player initialize called'
+    puts '-- Player initialize called --'
     @turn = 0
+    @minhealth = 20
   end
 
   def play_turn(warrior)
     @turn += 1
     puts "------------- Turn: #{@turn}"
 
+    if warrior.health < @minhealth
+      @minhealth = warrior.health
+    end
+    puts "  Min Health: #{@minhealth}"
+
     if warrior.feel.empty? &&
-       warrior.health < 20
+       warrior.health < 7
       warrior.rest!
       return
     end
